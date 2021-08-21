54fc1a22dbb35d23b968f6b618edb390dafba4b8
Aldyy123 <bocah.ngik@gmail.com>
2021-08-21 22:00:58 +0700

    adding animation

--- src/Components/LandingPage.jsx
+++ src/Components/LandingPage.jsx
@@ -14,26 +14,26 @@ export default class LandingPage extends Component {
 
                 <div className="landing coda">
                     <div>
-                        <p className="subjudul"  data-aos='fade-right' data-aos-duration='1500'>
+                        <p className="subjudul"  data-aos='fade-right' data-aos-duration='2500'>
                         Bukan hanya membuat UMKM beralih ke pemasaran online. Kami juga akan memberikan pelatihan selama 30 hari full dengan total 25 lebih materi serta diisi oleh 20 mentor yang berpengalaman dibidangnya.
                         </p>
-                        <p className="subjudul" data-aos='fade-right' data-aos-duration='1500'>
+                        <p className="subjudul" data-aos='fade-right' data-aos-duration='2500'>
                         Sehingga UMKM bukan hanya beralih ke pemasaran online, namun juga dapat membuat strategi pemasaran online yang sesuai dengan marketnya. Mulai dari konsep bisnis, build digital aset, branding, digital marketing, hingga scale up bisnis. 
                         </p>
-                        <p className="subjudul" data-aos='fade-right' data-aos-duration='1500'>
+                        <p className="subjudul" data-aos='fade-right' data-aos-duration='2500'>
                         Dengan begitu harapannya UMKM bukan hanya beralih ke pemasaran online, namun juga dapat bersaing dalam pemasaran di era digital.
                         </p>
-                        <div className="text-center"  data-aos='zoom-in-up' data-aos-duration='1500'>
+                        <div className="text-center"  data-aos='zoom-in-up' data-aos-duration='2500'>
                             <hr />
                             {<Countdown date={timeEvents} renderer={TimeCout} />}
                             <hr />
                         </div>
                     </div>
                     <div>
-                        <div data-aos='zoom-in' data-aos-duration='1500' data-aos-easing='ease-out-cubic'>
+                        <div data-aos='zoom-in' data-aos-duration='2500' data-aos-easing='ease-out-cubic'>
                             <img src={logo} className="logo" alt="logo"/>
                         </div>
-                        <div data-aos='zoom-in' data-aos-duration='1500' data-aos-easing='ease-out-cubic'>
+                        <div data-aos='zoom-in' data-aos-duration='2500' data-aos-easing='ease-out-cubic'>
                             <a href="/" className="button rounded">Amankan Kursi Sekarang</a>
                         </div>
                     </div>
